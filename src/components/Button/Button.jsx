8833c2645131ab8055380eab514a1649761017ab
iDgoe <e.novykov@gmail.com>
2023-05-05 20:53:11 +0300

    button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,24 @@
 import './Button.css';
 import PropTypes from 'prop-types';
 
+import React, { useState } from "react";
+
 export const Button = ({ onloadMore }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    await onloadMore();
+    setIsLoading(false);
+  };
+
   return (
-    <div className="Button-container" onClick={onloadMore}>
-      <button type="button" className="Button">
-        Load more
+    <div className="Button-container" onClick={handleClick}>
+      <button
+        type="button"
+        className={`Button ${isLoading ? "Button--loading" : ""}`}
+      >
+        {isLoading ? "Loading..." : "Load more"}
       </button>
     </div>
   );
